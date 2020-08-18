0f1cdb90447f6e3fb790b55792130dcc01571a42
Labham-Jain <coding.labham@gmail.com>
2020-08-18 14:57:34 +0530

    Initial Commit After Part 4 Of Video Tutorials

---
+++ four.js
@@ -0,0 +1,71 @@
+// ********************************
+// global objects
+
+// ------------
+
+// console
+
+// console.log("Hello, World!");
+
+// __dirname
+// console.log(__dirname);
+
+// __filename
+// console.log(__filename);
+
+// module
+
+// require
+// const add = require("./four_ref_file");
+
+// add(33, 89);
+
+// process
+// console.log(process.env);
+
+// setInterval
+
+// let i = 0;
+
+// const myInterval = setInterval(() => {
+// 	i++;
+
+// 	console.log(i);
+
+// 	if (i == 70) {
+// 		clearInterval(myInterval);
+// 	}
+// }, 100);
+
+// setTimeout
+
+// JSON
+
+// const myjson = JSON.stringify({ name: "foo" });
+
+// console.log(typeof JSON.parse(myjson));
+
+// ------------
+
+// ********************************
+
+// builtin modules
+
+// fs
+// fs == file system
+
+// const fs = require("fs");
+
+// path
+
+// const path = require("path");
+
+// http
+
+// const http = require("http");
+
+// http.createServer((req, res) => {
+// 	res.write("hello, world!");
+
+// 	res.end();
+// }).listen(7654);
---
+++ second.js
@@ -0,0 +1,52 @@
+// let, const
+// arrow functions
+// IIFE
+
+// let name = "rakesh";
+
+// const age = 2;
+
+// random function that change birthday date & increases 1 year more.
+
+// age = age + 1;
+
+// console.log(age);
+
+// function username() {
+// 	// 'let' is used for scope based variable declaration.
+// 	name = "shivam";
+
+// 	console.log(name);
+// }
+
+// username();
+
+// console.log(name);
+
+// function func1(age) {
+// 	console.log("Hello, From func1");
+// }
+
+// let func2 = (age) => {
+// 	age = age + 1;
+// 	return age;
+// };
+
+// func1();
+// console.log(func2());
+
+// IIFE
+
+// let USER;
+// (function () {
+// 	let user = "satyam sinha";
+// 	USER = user;
+// })();
+
+// let user = "shivam";
+
+// console.log(USER);
+
+// ((user)=>{
+
+// })(user);
